fd29cb854e4a1ef75197ab4e8e857474af7eef19
yitznewton <yitzchak.schaffer@gmx.com>
2010-05-12 12:36:33 -0400

    clean up IP notification form labels

--- lib/form/OrganizationForm.class.php
+++ lib/form/OrganizationForm.class.php
@@ -14,6 +14,12 @@ class OrganizationForm extends BaseOrganizationForm
     unset( $this['updated_at'] );
 
     $this->widgetSchema['alt_name']->setLabel('Alternate name');
+    $this->widgetSchema['ip_notification_method_id']->setLabel('IP notification method');
+    $this->widgetSchema['ip_notification_force_manual']->setLabel('Manual IP notification');
+    $this->widgetSchema['ip_notification_uri']->setLabel('IP notification URI');
+    $this->widgetSchema['ip_notification_username']->setLabel('IP notification username');
+    $this->widgetSchema['ip_notification_password']->setLabel('IP notification password');
+    $this->widgetSchema['ip_notification_contact_id']->setLabel('IP notification contact');
 
     $decorator = new freermsWidgetFormatterDiv($this->widgetSchema);
     $this->widgetSchema->addFormFormatter('div', $decorator);
