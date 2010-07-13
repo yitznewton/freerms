b79eccf9f180d51f2f4230c798b97617456bbd7d
yitznewton <yitzchak.schaffer@gmx.com>
2010-07-13 11:44:46 -0400

    refactor widget

--- lib/widget/freermsWidgetFormInputDeleteAdd.class.php
+++ lib/widget/freermsWidgetFormInputDeleteAdd.class.php
@@ -9,49 +9,44 @@ class freermsWidgetFormInputDeleteAdd extends sfWidgetFormInput
     $this->addOption('model_id');
     $this->addOption('confirm');
     $this->addOption('delete_action');
-    $this->addOption('delete_text', null);
+    $this->addOption('delete_text');
     $this->addOption('add_action');
-    $this->addOption('add_text', null);
+    $this->addOption('add_text');
   }
 
-  protected function renderLink()
+  protected function renderLinks()
   {
+    $controller = sfContext::getInstance()->getController();
     $html_bits = array();   
 
-    $delete_text = $this->getOption('delete_text');
+    $delete_text   = $this->getOption('delete_text');
+    $add_text   = $this->getOption('add_text');
 
-    $controller = sfContext::getInstance()->getController();
-    $delete_action = $controller->genUrl($this->getOption('delete_action'))
-      .'?id='.$this->getOption('model_id');
+    if ( $delete_text ) {
+      $delete_action = $controller->genUrl($this->getOption('delete_action'))
+                       .'?id='.$this->getOption('model_id');
 
-    $add_text = $this->getOption('add_text');
-    
-    $add_action = $this->getOption('add_action');      
+      $a_attributes_delete = array(
+        'href' => $delete_action,
+        'class' => 'input-link',
+        'onclick' => 'if (confirm(\''.$this->getOption('confirm').'\')) { return true; };return false;',
+      );
 
-    $a_attributes_delete = array(
-      'href' => $delete_action,
-      'class' => 'input-link',
-      'onclick' => 'if (confirm(\''.$this->getOption('confirm').'\')) { return true; };return false;',
-    );
+      $html_bits[] = $this->renderContentTag( 'a', $delete_text, $a_attributes_delete );
+    }
 
-    $a_attributes_add = array(
-      'href' => '#',
-      'class' => 'input-link',
-      'onclick' => 'return '. $add_action,
-    );
+    if ( $add_text ) {
+      $a_attributes_add = array(
+        'href' => '#',
+        'class' => 'input-link',
+        'onclick' => 'return ' . $this->getOption('add_action'),
+      );
+      
+      $html_bits[] = $this->renderContentTag( 'a', $add_text, $a_attributes_add );
+    }
      
-    $html_bits[0] = $this->renderContentTag( 'a', $delete_text, $a_attributes_delete );
 
-    $html_bits[1] = $this->renderContentTag( 'a', $add_text, $a_attributes_add );
-
-    if (!is_null($delete_text) && !is_null($add_text)){
-      $html = implode( ' | ', $html_bits );
-    }
-    else {
-      $html = implode(' ', $html_bits);
-    }
-        
-    return $html;    
+    return implode( ' | ', $html_bits );
   }
 
   public function render($name, $value = null, $attributes = array(), $errors = array())
@@ -63,6 +58,6 @@ class freermsWidgetFormInputDeleteAdd extends sfWidgetFormInput
     );
 
     return $this->renderTag('input', array_merge($baseAttributes, $attributes))
-      . "\n" . $this->renderLink();   
+           . "\n" . $this->renderLinks();
   }
 }
