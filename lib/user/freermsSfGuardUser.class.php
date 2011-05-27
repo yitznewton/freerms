c776e45225a057bc1aa8197a1e95c3923b9c09df
yitznewton <yitznewton@hotmail.com>
2011-05-27 12:56:42 -0400

    user classes missed in earlier commit

--- lib/user/freermsSfGuardUser.class.php
+++ lib/user/freermsSfGuardUser.class.php
@@ -1,14 +1,34 @@
 <?php
-
+/**
+ * FreERMS
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to yitznewton@hotmail.com so we can send you a copy immediately.
+ *
+ * @version x
+ * @package user
+ */
+/**
+ * freermsSfGuardUser is an Adapter for the sfGuardPlugin user class 
+ *
+ * @package user
+ * @copyright  Copyright (c) 2011 Benjamin Schaffer (http://yitznewton.org/)
+ */
 class freermsSfGuardUser extends sfGuardSecurityUser implements freermsUserInterface
 {
   public function getLibraryIds()
   {
-    //return $this->getGroups();
-  }
-  
-  public function getCredentials()
-  {
-    //return $this->getPermissions();
+    $library_ids = array();
+    
+    foreach ( $this->getGroups() as $group ) {
+      $library_ids[] = $group->getId();
+    }
+    
+    return $library_ids;
   }
 }
--- lib/user/freermsUserInterface.class.php
+++ lib/user/freermsUserInterface.class.php
@@ -1,8 +1,34 @@
 <?php
-
+/**
+ * FreERMS
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to yitznewton@hotmail.com so we can send you a copy immediately.
+ *
+ * @version x
+ * @package user
+ */
+/**
+ * freermsUserInterface specifies the interface for all user classes to be
+ * used in FreERMS
+ *
+ * @package user
+ * @copyright  Copyright (c) 2011 Benjamin Schaffer (http://yitznewton.org/)
+ */
 interface freermsUserInterface
 {
-  public function checkPassword($password);
+  /**
+   * @param string $password
+   * @return bool
+   */
+  public function checkPassword( $password );
+  /**
+   * @return array int[]
+   */
   public function getLibraryIds();
-  public function getCredentials();
 }
