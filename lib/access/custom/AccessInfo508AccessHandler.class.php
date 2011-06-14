ceaa2e301b070f212fe207b8f178757fbe8df68c
yitznewton <yitznewton@hotmail.com>
2011-06-14 16:00:37 -0400

    AccessHandler: fix bugs in Essential Evidence and related code

--- lib/access/custom/AccessInfo508AccessHandler.class.php
+++ lib/access/custom/AccessInfo508AccessHandler.class.php
@@ -6,11 +6,19 @@ class AccessInfo508AccessHandler extends EZproxyAccessHandler
 {
   const IS_VALID_ONSITE   = true;
   const IS_VALID_OFFSITE  = true;
-  const FORCE_AUTH_ONSITE = true;
   
   public function execute()
   {
-    if ( ! in_array( $this->action->getUser()->getProgramCode(), array('HS PA ','HS PAM') )) {
+    if ( ! $this->action->getUser()->isAuthenticated() ) {
+      $this->action->forward(
+        sfConfig::get('sf_login_module'), sfConfig::get('sf_login_action') );
+
+      return;
+    }
+    
+    $program = $this->action->getUser()->getProgramCode();
+    
+    if ( ! in_array( $program, array('HS PA ','HS PAM') )) {
       throw new freermsUnauthorizedException();
     }
 
