b145e81890d25de1eae528fe5cfc815a83bf96f6
yitznewton <yitznewton@hotmail.com>
2011-06-14 12:54:50 -0400

    admin subject: fix deleted_at bug in model; tweak css

--- lib/model/DbSubject.php
+++ lib/model/DbSubject.php
@@ -6,4 +6,16 @@ class DbSubject extends BaseDbSubject
   {
     return $this->getLabel();
   }  
+
+  public function getEResourceDbSubjectAssocs($criteria = null, PropelPDO $con = null)
+  {
+    if ( ! $criteria ) {
+      $criteria = new Criteria();
+    }
+    
+    $criteria->addJoin( EResourceDbSubjectAssocPeer::ER_ID, EResourcePeer::ID );
+    $criteria->add( EResourcePeer::DELETED_AT, null, Criteria::ISNULL );
+    
+    return parent::getEResourceDbSubjectAssocs( $criteria, $con );
+  }
 }
