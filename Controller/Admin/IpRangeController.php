1104293e200f773b79dfaebdb2c13358392edfca
yitznewton <yitznewton@hotmail.com>
2011-12-08 14:07:34 -0500

    move to YML-based routing

--- Controller/Admin/IpRangeController.php
+++ Controller/Admin/IpRangeController.php
@@ -2,26 +2,13 @@
 
 namespace Yitznewton\FreermsBundle\Controller\Admin;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use Yitznewton\FreermsBundle\Admin\IpRangeAdmin;
 
-/**
- * @Route("/ip")
- */
 class IpRangeController extends Controller
 {
-    /**
-     * @Route("/")
-     * @Template()
-     */
-    public function listAction()
+    public function indexAction()
     {
-        $view = 'YitznewtonFreermsBundle:Admin/IpRange:list.html.twig';
-        $content = $this->renderView($view);
-        return new Response($content);
+        return $this->render('YitznewtonFreermsBundle:Admin/IpRange:index.html.twig', array('name' => 'jimm'));
     }
 }
 
--- Tests/Controller/Admin/IpRangeControllerTest.php
+++ Tests/Controller/Admin/IpRangeControllerTest.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace Yitznewton\FreermsBundle\Tests\Controller;
+namespace Yitznewton\FreermsBundle\Tests\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class IpRangeControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+//         $crawler = $client->request('GET', '/hello/Fabien');
+// 
+//         $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
     }
 }
