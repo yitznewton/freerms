68901bade7eeda44d3022c73c217a2a553e24231
yitznewton <yitznewton@hotmail.com>
2011-12-22 12:48:02 -0500

    add test for IP address validator

---
+++ test/phpunit/unit/freermsValidatorIpAddressTest.php
@@ -0,0 +1,21 @@
+<?php
+require_once dirname(__FILE__).'/../bootstrap/unit.php';
+
+class unit_freermsValidatorIpAddressTest extends sfPHPUnitBaseTestCase
+{
+  public function testClean_ValidIp_NotThrows()
+  {
+    $validator = new freermsValidatorIpAddress();
+    $this->assertEquals('192.168.65.12', $validator->clean('192.168.65.12'));
+  }
+
+  /**
+   * @expectedException sfValidatorError
+   */
+  public function testClean_InvalidIp_Throws()
+  {
+    $validator = new freermsValidatorIpAddress();
+    $validator->clean('1922.168.65.12');
+  }
+}
+
