333520e345d81486097200dd56cb15cbaf4c49ce
yitznewton <yitznewton@hotmail.com>
2011-12-21 22:26:46 -0500

    use _sort columns in backend/IpRange

--- apps/backend/modules/ip_range/actions/actions.class.php
+++ apps/backend/modules/ip_range/actions/actions.class.php
@@ -13,4 +13,22 @@ require_once dirname(__FILE__).'/../lib/ip_rangeGeneratorHelper.class.php';
  */
 class ip_rangeActions extends autoIp_rangeActions
 {
+  protected function addSortQuery($query)
+  {
+    if (array(null, null) == ($sort = $this->getSort()))
+    {
+      return;
+    }
+
+    if (!in_array(strtolower($sort[1]), array('asc', 'desc')))
+    {
+      $sort[1] = 'asc';
+    }
+
+    if (substr($sort[0], -3) == '_ip') {
+      $sort[0] .= '_sort';
+    }
+
+    $query->addOrderBy($sort[0] . ' ' . $sort[1]);
+  }
 }
