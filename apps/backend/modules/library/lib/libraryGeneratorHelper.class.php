7afdfb9b1df6b9cbcb64cd4df01cacdbc456c150
yitznewton <yitznewton@hotmail.com>
2011-12-22 15:27:29 -0500

    add backend/Library

---
+++ apps/backend/modules/library/lib/libraryGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * library module helper.
+ *
+ * @package    freerms
+ * @subpackage library
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class libraryGeneratorHelper extends BaseLibraryGeneratorHelper
+{
+}
