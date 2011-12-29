f9c217d5fc40fc7088c44d8b9326fd930d856b9a
yitznewton <yitznewton@hotmail.com>
2011-12-29 17:09:17 -0500

    frontend: initial structure

---
+++ apps/frontend/modules/database/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * database actions.
+ *
+ * @package    freerms
+ * @subpackage database
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class databaseActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    return sfView::SUCCESS;
+  }
+}
