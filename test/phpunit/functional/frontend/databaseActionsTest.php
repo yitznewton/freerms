cdc22fdf2ddcfb3623f42c912ba949b0933f17d4
yitznewton <yitznewton@hotmail.com>
2012-01-04 17:23:52 -0500

    fix frontend/database func test API calls

--- test/phpunit/functional/frontend/databaseActionsTest.php
+++ test/phpunit/functional/frontend/databaseActionsTest.php
@@ -3,16 +3,36 @@ require_once dirname(__FILE__).'/../../bootstrap/functional.php';
 
 class functional_frontend_databaseActionsTest extends sfPHPUnitBaseFunctionalTestCase
 {
+  public static function setUpBeforeClass()
+  {
+    parent::setUpBeforeClass();
+
+    new sfDatabaseManager(
+      ProjectConfiguration::getApplicationConfiguration('admin', 'test', true));
+  }
+
+  public function setUp()
+  {
+    parent::setUp();
+
+    $doctrineInsert = new sfDoctrineDataLoadTask(
+      ProjectConfiguration::getActive()->getEventDispatcher(),
+      new sfAnsiColorFormatter());
+
+    $doctrineInsert->run(array('test/data/fixtures'), array("--env=test"));
+  }
+
   protected function getApplication()
   {
     return 'frontend';
   }
 
-  public function testDefault_NoArgs_GetsIndex()
+  public function testDefault_NoArgsOnsite_GetsIndex()
   {
-    $browser = $this->getBrowser();
+    $browser = new sfBrowser(null, '192.168.100.100');
+    $test = new sfTestFunctional($browser, $this->getTest());
 
-    $browser->
+    $test->
       get('/')->
 
       with('request')->begin()->
@@ -27,11 +47,12 @@ class functional_frontend_databaseActionsTest extends sfPHPUnitBaseFunctionalTes
     ;
   }
 
-  public function testDefault_WithSubject_GetsRightDatabase()
+  public function testDefault_WithSubjectOnsite_GetsRightDatabase()
   {
-    $browser = $this->getBrowser();
+    $browser = new sfBrowser(null, '192.168.100.100');
+    $test = new sfTestFunctional($browser, $this->getTest());
 
-    $browser->
+    $test->
       get('/?subject=health-sciences')->
 
       with('request')->begin()->
@@ -46,3 +67,4 @@ class functional_frontend_databaseActionsTest extends sfPHPUnitBaseFunctionalTes
     ;
   }
 }
+
