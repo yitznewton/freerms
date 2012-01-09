ec4a729e0ff3f32a744e61228ca71878270e8c73
yitznewton <yitznewton@hotmail.com>
2012-01-09 15:48:13 -0500

    backend: add nav

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html>
   <head>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
@@ -9,6 +9,15 @@
     <?php include_javascripts() ?>
   </head>
   <body>
+    <nav>
+      <ul>
+        <li><?php echo link_to('Databases', '@database') ?></li>
+        <li><?php echo link_to('Subjects', '@subject') ?></li>
+        <li><?php echo link_to('Libraries', '@library') ?></li>
+        <li><?php echo link_to('IP Ranges', '@ip_range') ?></li>
+      </ul>
+    </nav>
+    
     <?php echo $sf_content ?>
   </body>
 </html>
