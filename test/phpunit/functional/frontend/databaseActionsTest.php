f3cb03a44388606457f49171236273cc0d4b8853
yitznewton <yitznewton@hotmail.com>
2012-01-15 10:54:37 -0500

    frontend: fix ezproxy functional tests

--- test/phpunit/functional/frontend/databaseActionsTest.php
+++ test/phpunit/functional/frontend/databaseActionsTest.php
@@ -3,6 +3,24 @@ require_once dirname(__FILE__).'/FrontendFunctionalTestCase.php';
 
 class functional_frontend_databaseActionsTest extends FrontendFunctionalTestCase
 {
+  protected function login(sfTestFunctional $tester, $username, $password)
+  {
+    $csrf = $tester->getResponseDom()->getElementById('signin__csrf_token')
+      ->getAttribute('value');
+
+    $tester->post('/guard/login', array('signin' => array(
+      'username' => $username,
+      'password' => $password,
+      '_csrf_token' => $csrf,
+    )));
+
+    $tester->test()->is($tester->getResponse()->getStatusCode(), 302);
+
+    $tester->followRedirect();
+
+    return $tester;
+  }
+
   public function testIndex_NoArgsOnsite_GetsIndex()
   {
     $this->getTester('192.168.100.100')->
@@ -141,7 +159,7 @@ class functional_frontend_databaseActionsTest extends FrontendFunctionalTestCase
     ;
   }
 
-  public function testAccess_Ezproxy_RedirectHasProxyHost()
+  public function testAccess_EzproxyOnsite_RedirectsStraight()
   {
     $database = Doctrine_Core::getTable('Database')
       ->findOneByTitle('EZproxy database');
@@ -151,26 +169,56 @@ class functional_frontend_databaseActionsTest extends FrontendFunctionalTestCase
 
     $tester = $this->getTester('192.167.100.100');
 
-    $tester->
-      get('/database/' . $database->getId())->
+    $tester->get('/database/' . $database->getId());
 
-      with('request')->begin()->
-        isParameter('module', 'database')->
-        isParameter('action', 'access')->
-      end()->
+    $tester->test()->is($tester->getResponse()->getHttpHeader('Location'),
+      $database->getAccessUrl());
+  }
 
-      with('response')->begin()->
-        isStatusCode(302)->
-      end()
-    ;
+  public function testAccess_Ezproxy_RedirectHasProxyHost()
+  {
+    $database = Doctrine_Core::getTable('Database')
+      ->findOneByTitle('EZproxy database');
+
+    $library = Doctrine_Core::getTable('Library')
+      ->findOneByCode('TCS');
+
+    $user = Doctrine_Core::getTable('sfGuardUser')
+      ->findOneByUsername('haslibrarytcs');
+
+    $tester = $this->getTester('192.1.1.1');
+
+    $tester->get('/database/' . $database->getId());
+
+    $tester = $this->login($tester, 'haslibrarytcs', 'jimbobjoe');
+
+    $tester->test()->is($tester->getResponse()->getStatusCode(), 302);
 
     $tester->test()->like($tester->getResponse()
-      ->getHttpHeader('Location'), "/{$library->getEzproxyHost()}/");
+      ->getHttpHeader('Location'), "`http://{$library->getEzproxyHost()}`");
   }
 
   public function testAccess_Ezproxy_RedirectHasUsername()
   {
-    $this->markTestSkipped();
+    $database = Doctrine_Core::getTable('Database')
+      ->findOneByTitle('EZproxy database');
+
+    $library = Doctrine_Core::getTable('Library')
+      ->findOneByCode('TCS');
+
+    $user = Doctrine_Core::getTable('sfGuardUser')
+      ->findOneByUsername('haslibrarytcs');
+
+    $tester = $this->getTester('192.1.1.1');
+
+    $tester->get('/database/' . $database->getId());
+
+    $tester = $this->login($tester, 'haslibrarytcs', 'jimbobjoe');
+
+    $tester->test()->is($tester->getResponse()->getStatusCode(), 302);
+
+    $tester->test()->like($tester->getResponse()
+      ->getHttpHeader('Location'), '/user=haslibrarytcs/');
   }
 
   public function testAccess_Ezproxy_RedirectHasUrl()
@@ -181,14 +229,37 @@ class functional_frontend_databaseActionsTest extends FrontendFunctionalTestCase
     $library = Doctrine_Core::getTable('Library')
       ->findOneByCode('TCS');
 
+    $user = Doctrine_Core::getTable('sfGuardUser')
+      ->findOneByUsername('haslibrarytcs');
+
+    $tester = $this->getTester('192.1.1.1');
+
+    $tester->get('/database/' . $database->getId());
+
+    $tester = $this->login($tester, 'haslibrarytcs', 'jimbobjoe');
+
+    $tester->test()->is($tester->getResponse()->getStatusCode(), 302);
+
+    $tester->test()->like($tester->getResponse()
+      ->getHttpHeader('Location'), "`url={$database->getAccessUrl()}`");
+  }
+
+  public function testAccess_EzproxyUrl_InvalidUrlThrows404()
+  {
+    $this->markTestSkipped();
+  }
+
+  public function testAccess_EzproxyUrl_RedirectHasUrl()
+  {
+    $this->markTestSkipped();
     $tester = $this->getTester('192.167.100.100');
 
-    $tester->
-      get('/database/' . $database->getId())->
+    $url = 'http://www.example.org/';
 
+    $tester->get("/url/$url")->
       with('request')->begin()->
-        isParameter('module', 'database')->
-        isParameter('action', 'access')->
+        isParameter('module', 'access')->
+        isParameter('action', 'ezproxyUrlAccess')->
       end()->
 
       with('response')->begin()->
@@ -197,7 +268,7 @@ class functional_frontend_databaseActionsTest extends FrontendFunctionalTestCase
     ;
 
     $tester->test()->like($tester->getResponse()
-      ->getHttpHeader('Location'), "_{$database->getAccessUrl()}_");
+      ->getHttpHeader('Location'), "_{$url}_");
   }
 }
 
