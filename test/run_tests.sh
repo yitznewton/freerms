14cf2726b5b468369a4e0ca6256bd0a0777d53ca
yitznewton <yitznewton@hotmail.com>
2012-01-18 15:58:04 -0500

    fix test script

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -2,5 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-find $DIR/test/phpunit -name *Test.php -print -exec phpunit --colors {} \;
+find $DIR/phpunit -name *Test.php -print -exec phpunit --colors {} \;
 
