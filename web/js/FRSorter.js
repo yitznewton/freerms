7d6279778ca1651652f5ed426899d424e0c14948
yitznewton <yitznewton@hotmail.com>
2012-02-02 16:26:53 -0500

    rewrite FRSorter API

---
+++ web/js/FRSorter.js
@@ -0,0 +1,34 @@
+/**
+ * Represents a jQuery UI Sortable
+ *
+ * @constructor
+ * @param {string} id - The desired ID for the <ul>
+ */
+function FRSorter(id)
+{
+}
+
+/**
+ * @param {string} v
+ */
+FRSorter.prototype.setWeighted = function(v) {
+}
+
+/**
+ * @param {array} FRSorter[] connections
+ */
+FRSorter.prototype.setConnections = function(connections) {
+}
+
+/**
+ * @param {FRSorterRow} row
+ */
+FRSorter.prototype.pushRow = function(row) {
+}
+
+/**
+ * @return {HTMLUListElement}
+ */
+FRSorter.prototype.render = function() {
+}
+
--- web/js/backend.js
+++ web/js/backend.js
@@ -1,9 +1,71 @@
 var FRBackend = {
+  rootUrl: function() {
+    var url_matches = window.location.href.match(/^.+backend[^\.]*\.php/);
+    
+    if (url_matches) {
+      return url_matches[0];
+    }
+    else {
+      throw new Error('Could not parse URL');
+    }
+  }
 };
 
 $(document).ready(function() {
+  // setup subject sorters
+
+  var databaseFormContainer = FR.$$('sf_admin_form_field_DatabaseSubject');
+
+  if (databaseFormContainer) {
+    var nonfeatured_sorter = new FRSorter('databases-nonfeatured');
+    var featured_sorter    = new FRSorter('databases-featured');
+
+    featured_sorter.setWeighted(true);
+    featured_sorter.setConnections([nonfeatured_sorter]);
+    nonfeatured_sorter.setWeighted(false);
+    nonfeatured_sorter.setConnections([featured_sorter]);
+    
+    var subjectId = FR.$$('subject_id').value;
+
+    $('input', databaseFormContainer).each( function() {
+      var row  = new FRSorterRow('sometitle', this);
+      var erId = 'somemungedthing';
+
+      //FIXME relative URL
+      var url = '/subject/ajax/remove/er_id/' + erId
+                + '/subject_id/' + subjectId;
+
+      row.setAjaxOnRemove(url);
+
+      if (row.getWeight() === -1) {
+        nonfeatured_sorter.pushRow(row);
+      }
+      else {
+        featured_sorter.pushRow(row); 
+      }
+    });
+    
+    $(databaseFormContainer)
+      .append($('<h3>Featured databases</h3>'))
+      .append(featured_sorter.render())
+      .append($('<h3>Non-featured databases</h3>'))
+      .append(nonfeatured_sorter.render())
+      ;
+
+    // FIXME do we have to wait til here to hide this?
+    $('table', databaseFormContainer).hide();
+
+    $('#sf_admin_content form').submit( function() {
+      featured_sorter.bindInputs();
+      nonfeatured_sorter.bindInputs();
+    });
+  }
+
+  // setup form dirty detection
   var forms = document.getElementsByTagName('FORM');
   var form = forms[0];
+
+  form.isDirty = false;
 
   $.each(document.getElementsByTagName('INPUT'), function() {
     this.onchange = function() {
