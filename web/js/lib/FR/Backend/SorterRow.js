3bbddf1794f1faa9eaefeba3be6439a3ad1a27ba
yitznewton <yitznewton@hotmail.com>
2012-02-07 13:45:03 -0500

    more tests

--- web/js/lib/FR/Backend/SorterRow.js
+++ web/js/lib/FR/Backend/SorterRow.js
@@ -14,6 +14,18 @@ FR.Backend.SorterRow = function(title, inputEl) {
    * @type HTMLInputElement
    */
   this.inputEl;
+  /**
+   * @type HTMLLiElement
+   */
+  this.liEL;
+  /**
+   * @type Boolean
+   */
+  this.isRendered = false;
+  /**
+   * @type function
+   */
+  this.onRemove;
 
   if (title.constructor != String) {
     throw new Error('title must be a string');
@@ -25,25 +37,58 @@ FR.Backend.SorterRow = function(title, inputEl) {
 
   this.title   = title;
   this.inputEl = inputEl;
+  this.liEl    = document.createElement('li');
 };
 
 /**
+ * @param {Object} options
  * @returns {HTMLLIElement}
  */
-FR.Backend.SorterRow.prototype.render = function() {
-  var li = document.createElement('li');
+FR.Backend.SorterRow.prototype.render = function(options) {
+  if (options === undefined) {
+    options = {};
+  }
 
-  li.className = 'ui-state-default';
-  li.innerHTML = this.title;
-  li.sorterRow = this;  // needed for binding
+  this.liEl.className = 'ui-state-default';
+  this.liEl.innerHTML = this.title;
+  this.liEl.sorterRow = this;  // needed for binding
 
-  var span_arrow = document.createElement('span');
-  span_arrow.className = 'ui-icon ui-icon-arrowthick-2-n-s';
-  li.appendChild(span_arrow);
+  var spanArrow = document.createElement('span');
+  spanArrow.className = 'ui-icon ui-icon-arrowthick-2-n-s';
+  this.liEl.appendChild(spanArrow);
 
-  // TODO: add arrow, close
+  var spanClose = options.spanClose;
 
-  return li;
+  if (!spanClose) {
+    spanClose = document.createElement('span');
+    spanClose.className = 'ui-icon ui-icon-close';
+  }
+
+  spanClose.onclick = function(sorterRow) {
+    return function() {
+      if (sorterRow.onRemove) {
+        sorterRow.onRemove();
+      }
+
+      sorterRow.remove();
+    }
+  }(this)
+
+  this.liEl.appendChild(spanClose);
+  this.isRendered = true;
+
+  return this.liEl;
+};
+
+/**
+ * @param {function} toFire
+ */
+FR.Backend.SorterRow.prototype.setOnRemove = function(onRemove) {
+  if (onRemove.constructor != Function) {
+    throw new Error('onRemove must be a Function');
+  }
+
+  this.onRemove = onRemove;
 };
 
 /**
@@ -64,9 +109,15 @@ FR.Backend.SorterRow.prototype.getWeight = function() {
   return parseInt(this.inputEl.value);
 };
 
-/**
- * @param {string} url
- */
-FR.Backend.SorterRow.prototype.setAjaxOnRemove = function(url) {
-};
+FR.Backend.SorterRow.prototype.remove = function() {
+  if (!this.isRendered) {
+    return;
+  }
+
+  $(this.liEl).slideUp('fast', function() {
+    if (this.parentNode) {
+      this.parentNode.removeChild(this);
+    }
+  });
+}
 
--- web/js/test/FR/Backend/SorterRowTest.js
+++ web/js/test/FR/Backend/SorterRowTest.js
@@ -32,6 +32,49 @@ SorterRowTest.prototype.testRender_ExpectedTitle = function() {
   var inputEl = document.createElement('input');
   var row     = new FR.Backend.SorterRow('foo', inputEl);
 
-  assertEquals('foo', row.render().innerHTML);
+  assertEquals('foo', row.render().innerHTML.substr(0,3));
+};
+
+SorterRowTest.prototype.testSetOnRemove_Invalid_ThrowsError = function() {
+  var inputEl = document.createElement('input');
+  var row     = new FR.Backend.SorterRow('foo', inputEl);
+
+  assertException(function() {
+    row.setOnRemove('sdfd');
+  });
+};
+
+SorterRowTest.prototype.testRemove_EventFires_RemovesLi = function() {
+  var inputEl = document.createElement('input');
+  var row     = new FR.Backend.SorterRow('foo', inputEl);
+
+  var spanClose = document.createElement('span');
+  spanClose.className = 'ui-icon ui-icon-close';
+
+  var ulEl = document.createElement('ul');
+  ulEl.appendChild(row.render({spanClose: spanClose}));
+
+  assertEquals(1, ulEl.childNodes.length);
+
+  jQuery(spanClose).click();
+
+  assertEquals(0, ulEl.childNodes.length);
+};
+
+SorterRowTest.prototype.testRemove_FiresOnRemove = function() {
+  var inputEl = document.createElement('input');
+  var row     = new FR.Backend.SorterRow('foo', inputEl);
+
+  var spanClose = document.createElement('span');
+  spanClose.className = 'ui-icon ui-icon-close';
+
+  var spy = sinon.spy();
+
+  row.setOnRemove(spy);
+  row.render({spanClose: spanClose});
+
+  jQuery(spanClose).click();
+
+  assertTrue(spy.called);
 };
 
