f43f8c3cefea764f84fcc51fad9ed2ce506c2ff1
yitznewton <yitznewton@hotmail.com>
2012-02-29 15:27:16 -0500

    fix backend database form templates

--- apps/backend/modules/database/templates/_form_header.php
+++ apps/backend/modules/database/templates/_form_header.php
@@ -1,7 +1,9 @@
+<?php if (!$database->isNew()): ?>
 <div class="user-url">
   User URL:
   <?php echo link_to(
     public_path('/database/' . $database->getId(), true),
     public_path('/database/' . $database->getId(), true)) ?> 
 </div>
+<?php endif; ?>
 
