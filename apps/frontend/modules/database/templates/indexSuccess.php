0524cc1f9fd02e4d2cd5ae6cfbd5bf009e11d810
yitznewton <yitznewton@hotmail.com>
2012-03-01 12:06:33 -0500

    revert cache key change

--- apps/frontend/modules/database/templates/indexSuccess.php
+++ apps/frontend/modules/database/templates/indexSuccess.php
@@ -1,15 +1,11 @@
-<?php use_helper('Frontend') ?>
-
 <h2>Databases</h2>
 
 <?php include_partial('subjectWidget', array(
   'widget' => $subjectWidget, 'default' => $subjectDefault)) ?>
 
 <?php include_component('database', 'featuredDatabases',
-  array('subject' => $subject, 'libraryIds' => $libraryIds,
-    'sf_cache_key' => cache_key($subject, $libraryIds))) ?>
+  array('subject' => $subject, 'libraryIds' => $libraryIds)) ?>
 
 <?php include_component('database', 'databases',
-  array('subject' => $subject, 'libraryIds' => $libraryIds,
-    'sf_cache_key' => cache_key($subject, $libraryIds))) ?>
+  array('subject' => $subject, 'libraryIds' => $libraryIds)) ?>
 
