f177a85b3836a61d8d7c15b31b230739c4224008
yitznewton <yitznewton@hotmail.com>
2012-03-01 15:52:00 -0500

    cleanup frontend js

--- web/js/lib/FR.js
+++ web/js/lib/FR.js
@@ -4,15 +4,6 @@ FR.$$ = function(id) {
   return document.getElementById( id );
 };
 
-FR.slugify = function(string) {
-  string = string.toLowerCase()
-    .replace(/\W/g, ' ')
-    .replace(/ +/g, '-')
-    .replace(/^-|-$/g, '')
-  
-  return string;
-};
-
 FR.trim = function(v) {
   return v.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 };
