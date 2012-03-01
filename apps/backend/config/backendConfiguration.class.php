a163923b5388e2d6a48bb3e9917c15bd68d29cd7
yitznewton <yitznewton@hotmail.com>
2012-03-01 14:34:41 -0500

    fix backend update cache clear for testing

--- apps/backend/config/backendConfiguration.class.php
+++ apps/backend/config/backendConfiguration.class.php
@@ -5,22 +5,24 @@ class backendConfiguration extends sfApplicationConfiguration
   public function configure()
   {
     // when updating a Database or Subject, or featured, clear frontend
-    // cache
-    $this->dispatcher->connect('admin.save_object', function($event) {
-      switch (get_class($event->offsetGet('object'))) {
-        case 'Database':
-        case 'Subject':
-          $frontendCache = new sfFileCache(array(
-            'cache_dir' => sfConfig::get('sf_cache_dir') . '/frontend/'
-                           . sfConfig::get('sf_environment') . '/template'));
+    // cache. breaks PHPUnit
+    if ($this->getEnvironment() !== 'test') {
+      $this->dispatcher->connect('admin.save_object', function($event) {
+        switch (get_class($event->offsetGet('object'))) {
+          case 'Database':
+          case 'Subject':
+            $frontendCache = new sfFileCache(array(
+              'cache_dir' => sfConfig::get('sf_cache_dir') . '/frontend/'
+                             . sfConfig::get('sf_environment') . '/template'));
 
-          $frontendCache->clean();
-          return;
+            $frontendCache->clean();
+            return;
 
-        default:
-          // do nothing
-          return;
-      }
-    });
+          default:
+            // do nothing
+            return;
+        }
+      });
+    }
   }
 }
