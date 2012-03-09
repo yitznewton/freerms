d452a29f1aaedf6b5e801e254c785de0e7d14dd0
yitznewton <yitznewton@hotmail.com>
2012-03-09 13:07:25 -0500

    add 500 error page

---
+++ config/error/error.html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Error | FreERMS</title>
+</head>
+<body>
+  <h2>Error</h2>
+
+  <p>We're sorry, our server encountered an unexpected error. Please try
+  again later, and file a technical support request if the problem
+  recurs.</p>
+</body>
+</html>
+
