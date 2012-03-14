7ccec01cf1ee3b157c1ac38fd7cffa8ae9c24951
yitznewton <yitznewton@hotmail.com>
2012-03-14 11:44:29 -0400

    refine subject widget query

--- lib/widget/WidgetFormChoiceSubject.class.php
+++ lib/widget/WidgetFormChoiceSubject.class.php
@@ -18,11 +18,12 @@ class WidgetFormChoiceSubject extends sfWidgetFormDoctrineChoice
     $this->setOption('key_method', 'getSlug');
     
     $q = Doctrine_Core::getTable('Subject')->createQuery('s')
+      ->select('s.id, s.name, s.slug')
+      ->distinct()
       ->leftJoin('s.Databases d')
       ->where('d.is_hidden = false')
       ->orderBy('s.name')
       ;
-    //$c->add( DbSubjectPeer::LABEL, 'HOME', Criteria::NOT_EQUAL );
 
     if (isset($options['library_ids']) && $options['library_ids']) {
       $q->leftJoin('d.Libraries l')
