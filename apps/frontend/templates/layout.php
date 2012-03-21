47506ab456e12563a865c97981182c7ba2dabdd4
yitznewton <yitznewton@hotmail.com>
2012-03-21 15:33:42 -0400

    html5 shiv for all IE browsers

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -5,7 +5,7 @@
     <?php include_metas() ?>
     <?php include_title() ?>
     <link rel="shortcut icon" href="/favicon.ico" />
-    <!--[if lt IE 9]
+    <!--[if IE]
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
     <?php include_stylesheets() ?>
